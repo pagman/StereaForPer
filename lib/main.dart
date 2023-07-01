1ce772786dac0f1b381e2a743af4b301dc172b21
panagiotismantas <mantas.pan1@gmail.com>
2023-07-01 11:35:58 +0300

    all assets added

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import 'HomePage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: '/',
+        routes: {
+          // When navigating to the "/" route, build the FirstScreen widget.
+          '/': (context) => const HomePage(),
+          // When navigating to the "/second" route, build the SecondScreen widget.
+
+        }
+    );
+  }
+}
+
