411ea8ce7c7692391af590244f0a0a3ed1cc87a3
panagiotismantas <mantas.pan1@gmail.com>
2023-07-01 12:26:09 +0300

    homepage base

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -101,7 +101,7 @@ class _HomePageState extends State<HomePage> {
                           elevation: 6,
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(20)),
-                          child: Center(
+                          child: Center(//sdfsdf
                             child: Text(
                               "Card ${i + 1}",
                               style: TextStyle(fontSize: 32),
